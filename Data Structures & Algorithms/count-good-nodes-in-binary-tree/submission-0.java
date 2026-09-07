802ba6ca4898b26a1fddbe50580e6c160e535485
vato <shrivathsa1551@gmail.com>
2026-09-07 11:41:51 +0530

    Add: count-good-nodes-in-binary-tree - submission-0

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int goodNodes(TreeNode root) {
+        return goodNodes(root, Integer.MIN_VALUE);
+    }
+
+    public int goodNodes(TreeNode root, int max) {
+        if (root == null) return 0;
+        int res = root.val >= max ? 1 : 0;
+        res += goodNodes(root.left, Math.max(max, root.val));
+        res += goodNodes(root.right, Math.max(max, root.val));
+        return res;
+    }
+}
